207ed9a6a81963274b6c814c483f438c88cae53d
Ruan <75811206+R-uan@users.noreply.github.com>
2024-07-31 00:08:16 -0300

    Somehow get rust into this
    this commit is just a way to make myself commited into making this cli work

---
+++ cli/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+//  weather task hourly/daily/... <boolean> (activates or desactivates a task from running)
+//  weather run hourly/daily/... (immediatly runs the task)
+//  weather add-city name lat lon (add a new city to the list :: should check coord types)
+//  ...
+//
+//
+//
+//
